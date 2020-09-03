4cde84a809785da9f2463c9068672d9a9fa757b1
LEFRAY <damienlefray@gmail.com>
2020-09-03 14:58:10 +0200

    maj 03.09.2020 15h

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -1,6 +1,23 @@
 module.exports = {
     get: (req, res) => {
+        const title = 'Mon Super titre'
+        const myArray = [
+            {name: 'name 1', description: 'desxcrerg'},
+            {name: 'name 2', description: 'desxcrerg'},
+            {name: 'name 3', description: 'desxcrerg'},
+            {name: 'name 4', description: 'desxcrerg'},
+        ]
         console.log('get home')
-        res.render('home')
+        res.render('home', {
+            success: "success",
+            titre: title,
+            tableau: myArray
+        })
     }
 }
+
+// titre   a inscerer dans une page en {{ titre }}
+// title   est = a la const title "ce qu'il y a a afficher dans la page"
+// myArray   cree les nombre d'acction demander ici 4 avec "name" et "description"
+
+// succes orange est envoyer a navbar.hbs
